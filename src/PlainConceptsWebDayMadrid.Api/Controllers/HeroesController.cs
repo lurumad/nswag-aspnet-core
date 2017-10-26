bab3f4096a36a0f2fdf943bc49d9c02bd66d0ca5
Luis Ruiz Pavón <luis_ruiz_pavon@hotmail.com>
2017-10-26 17:42:51 +0200

    Initial commit

---
+++ src/PlainConceptsWebDayMadrid.Api/Controllers/HeroesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using PlainConceptsWebDayMadrid.Api.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace PlainConceptsWebDayMadrid.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class HeroesController : ControllerBase
+    {
+        private List<Hero> heroes = new List<Hero>
+        {
+            new Hero{ Id = 11, Title = "Mr. Nice" },
+            new Hero{ Id = 12, Title = "Narco" },
+            new Hero{ Id = 13, Title = "Bombasto" },
+            new Hero{ Id = 14, Title = "Celeritas" },
+            new Hero{ Id = 15, Title = "Magneta" },
+            new Hero{ Id = 16, Title = "RubberMan" },
+            new Hero{ Id = 17, Title = "Dynama" },
+            new Hero{ Id = 18, Title = "Dr IQ" },
+            new Hero{ Id = 19, Title = "Magma" },
+            new Hero{ Id = 20, Title = "Tornado" }
+        };
+
+        [HttpGet]
+        [SwaggerResponse(HttpStatusCode.OK, typeof(List<Hero>))]
+        public IActionResult Get()
+        {
+            return Ok(heroes);
+        }
+
+        [HttpGet("{id:int}")]
+        [SwaggerResponse(HttpStatusCode.OK, typeof(Hero))]
+        public IActionResult GetBy(int id)
+        {
+            return Ok(null);
+        }
+    }
+}
---
+++ src/PlainConceptsWebDayMadrid.Api/Model/Hero.cs
@@ -0,0 +1,8 @@
+﻿namespace PlainConceptsWebDayMadrid.Api.Model
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
